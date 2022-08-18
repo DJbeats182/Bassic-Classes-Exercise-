8eee918b7b65ae8f0fb720fab173d57078c358f2
DJ Bates <blooviper@gmail.com>
2022-08-17 18:26:02 -0600

    First Commit

---
+++ Basic Classes Exercise/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Basic_Classes_Exercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new Car();
+            myCar.Make = "Tesla";
+            myCar.Model = "Model X";
+            myCar.Year = 2023;
+
+            var myCar2 = new Car()
+            {
+                Make = "Honda",
+                Model = "Civic",
+                Year = 2020,
+            };
+
+            Car myCar3 = new Car() {Make= "Audi", Model= "R8", Year= 2022 };
+
+            var carList = new List<Car>() { myCar, myCar2, myCar3 };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{vehicle.Make}-{vehicle.Model}-{vehicle.Year}");
+            }
+        }
+    }
+}
